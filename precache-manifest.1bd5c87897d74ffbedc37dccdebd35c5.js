3e53650ccf402bdc215c13ed4bd377a48f1467ab
p32929 <p32929@yahoo.com>
2020-06-25 17:37:45 +0600

    Updates

---
+++ precache-manifest.1bd5c87897d74ffbedc37dccdebd35c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a30c230cdc254dca19cd8dd11764c5",
+    "url": "/MyReactBoilerplate/index.html"
+  },
+  {
+    "revision": "d57a207711687c3c223e",
+    "url": "/MyReactBoilerplate/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7abf87f2324d0256b17",
+    "url": "/MyReactBoilerplate/static/js/2.4535b27b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MyReactBoilerplate/static/js/2.4535b27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57a207711687c3c223e",
+    "url": "/MyReactBoilerplate/static/js/main.4a92254a.chunk.js"
+  },
+  {
+    "revision": "b84f8a187f5331d14062",
+    "url": "/MyReactBoilerplate/static/js/runtime-main.ca745e91.js"
+  }
+]);
