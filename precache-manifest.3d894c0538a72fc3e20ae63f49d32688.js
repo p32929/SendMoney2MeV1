50e05f7a05c47ecc2eeb14b54005bf18d2000b46
p32929 <p32929@yahoo.com>
2020-07-08 01:24:50 +0600

    Updates

--- precache-manifest.3d894c0538a72fc3e20ae63f49d32688.js
+++ precache-manifest.3d894c0538a72fc3e20ae63f49d32688.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b12edd3798251d849bb4c8a8d7f196ba",
+    "revision": "9041b949235099c624869f639f2e5d38",
     "url": "/SendMoney2Me/index.html"
   },
   {
-    "revision": "3ef45bec90c4c3d2935e",
+    "revision": "a6ff8337c2a881fade06",
     "url": "/SendMoney2Me/static/css/main.69eda509.chunk.css"
   },
   {
-    "revision": "03afed1748839fda9b49",
-    "url": "/SendMoney2Me/static/js/2.58296efc.chunk.js"
+    "revision": "1356a158e14488ccf900",
+    "url": "/SendMoney2Me/static/js/2.5506851b.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/SendMoney2Me/static/js/2.58296efc.chunk.js.LICENSE.txt"
+    "url": "/SendMoney2Me/static/js/2.5506851b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3ef45bec90c4c3d2935e",
-    "url": "/SendMoney2Me/static/js/main.df4f6b0f.chunk.js"
+    "revision": "a6ff8337c2a881fade06",
+    "url": "/SendMoney2Me/static/js/main.bb300b29.chunk.js"
   },
   {
     "revision": "a1dd6ef061ceef7dbdaf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SendMoney2Me/precache-manifest.ee2f948a23ca43b070029a7380579838.js"
+  "/SendMoney2Me/precache-manifest.3d894c0538a72fc3e20ae63f49d32688.js"
 );
 
 self.addEventListener('message', (event) => {
